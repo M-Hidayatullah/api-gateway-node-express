99ebfb04bba847027e6d4e237eb9177153683d9e
M-Hidayatullah <hidayatullahblk@gmail.com>
2022-02-25 18:40:28 +0800

    create api get list image

--- routes/media.js
+++ routes/media.js
@@ -5,6 +5,22 @@ const base64Img = require('base64-img');
 
 const { Media } = require('../models');
 
+router.get('/', async(req, res) => {
+    const media = await Media.findAll({
+        attributes: ['id', 'image']
+    });
+
+    const mappedMedia = media.map((m) => {
+        m.image = `${req.get('host')}/${m.image}`;
+        return m;
+    });
+
+    return res.json({
+        status: 'success',
+        data: mappedMedia
+    });
+});
+
 router.post('/', (req, res) => {
     const image = req.body.image;
 
@@ -24,7 +40,7 @@ router.post('/', (req, res) => {
 
         const fileName = filepath.split("\\").pop().split("/").pop();
 
-        const media = await Media.create({ image: `image/${fileName}` });
+        const media = await Media.create({ image: `images/${fileName}` });
 
         return res.json({
             status: 'success',
