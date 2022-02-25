22375cf3306e0c5a231dd8d665d630c65976bd1a
M-Hidayatullah <hidayatullahblk@gmail.com>
2022-02-25 18:14:34 +0800

    create api post image

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const mediaRouter = require('./routes/media');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { Media } = require('../models');
+
+router.post('/', (req, res) => {
+    const image = req.body.image;
+
+    if (!isBase64(image, { mimeRequired: true })) {
+        res.status(400).json({
+            status: 'error',
+            message: 'Image is not base64',
+        });
+    }
+    base64Img.img(image, './public/images', Date.now(), async function(err, filepath) {
+        if (err) {
+            return res.status(400).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+
+        const fileName = filepath.split("\\").pop().split("/").pop();
+
+        const media = await Media.create({ image: `image/${fileName}` });
+
+        return res.json({
+            status: 'success',
+            data: {
+                id: media.id,
+                image: `${req.get('host')}/images/${fileName}`,
+            }
+        })
+
+    });
 });
 
 module.exports = router;
